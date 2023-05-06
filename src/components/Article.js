6aea6fff877de539c38677b8e793b0de8a521063
Damian <kuciel.damian2@gmail.com>
2023-05-06 16:48:56 +0200

    Fetch data from Api

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,11 +4,13 @@ import attach from '../assets/attach.png';
 import like from '../assets/like.png';
 import share from '../assets/share.png';
 
-
 const Article = (props) => {
+  const handleClick = () => {
+    window.open(props.link, '_blank');
+  }
+
   return (
     <div className="article">
-  
       <div className="article-header">
         <img src={attach} width="auto" className="article-icon" alt="attachment" />
         <h2 className="article-title">{props.title}</h2>
@@ -17,16 +19,12 @@ const Article = (props) => {
       <div className="article-info">
         <img src={props.image} className="article-image" alt="article" />
         <p className="article-description">{props.description}</p>
-        <button className="article-button">Go to Article</button>
+        <button className="article-button" onClick={handleClick}>Go to Article</button>
         <img src={like} className="article-share" alt="like" />
         <img src={share} className="article-like" alt="share" />
       </div>
     </div>
-
   );
 };
 
 export default Article;
-
-
-
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,54 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/style.css';
 import Article from './Article';
 import Sidebar from './Sidebar';
 import Navigation from './Navigation';
 import art from '../assets/art.png';
 
+import axios from 'axios';
+
 const Dashboard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/summarizer/api/v1')
+      .then((response) => {
+        setArticles(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="container">
       <Navigation /> 
       <div className="articles">
-        <Article
-          title="Lorem ipsum dolor sit amet"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          image={art}
-        />
-        <Article
-          title="Lorem ipsum dolor sit amet"
-          description="Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          image={art}
-        />
-        <Article
-          title="Sed do eiusmod tempor"
-          description="Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-          image={art}
-        />
-         <Article
-          title="Sed do eiusmod tempor"
-          description="Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-          image={art}
-        />
-         <Article
-          title="Sed do eiusmod tempor"
-          description="Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-          image={art}
-        />
-         <Article
-          title="Sed do eiusmod tempor"
-          description="Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-          image={art}
-        />
-         <Article
-          title="Sed do eiusmod tempor"
-          description="Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-          image={art}
-        />
+        {articles.map((article) => (
+          <Article
+            title={article.title}
+            description={article.content}
+            image={art}
+            link={article.link}
+          />
+        ))}
       </div>
       <Sidebar />
-      </div>
+    </div>
   );
 };
 
